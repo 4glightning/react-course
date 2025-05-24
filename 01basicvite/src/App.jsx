4650a9c58cf67f8ed5d4f95a430f6782bab4afbe
Muhammad Pauzi <85352895+4glightning@users.noreply.github.com>
2025-05-24 18:05:28 +0800

    Day 1
    reaching 1 hour checkpoint from the tutorial

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,15 @@
+import Youtube from "./youtube"
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello Vite react boi</h1>
+      <h1>Hello Vite react boi</h1>
+      <Youtube />
+    </>
+    
+  )
+}
+
+export default App
